e83bcfe5e01b9c594e7e026a889893c7725f9acb
TaylorArmstrong <TArmstrong1327@gmail.com>
2018-11-25 20:36:55 -0700

    initial create-app setup

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import SearchBar from '../search-bar/search-bar.js'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <SearchBar />
+      </main>
+    )
+  }
+}
+
+export default App
