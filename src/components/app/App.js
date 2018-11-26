9408580268efebc0e85edfbbc88cc4a898b47281
TaylorArmstrong <TArmstrong1327@gmail.com>
2018-11-25 21:21:40 -0700

    api request working and search bar setup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,47 @@ import SearchBar from '../search-bar/search-bar.js'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.apiBase = 'http://localhost:8082/api/books'
+    this.state = {
+      books: [],
+      searchInput: ''
+    }
+  }
+
+ async componentDidMount() {
+   const response = await fetch(this.apiBase)
+   
+   if (response.status === 200) {
+     const resJSON = await response.json()
+     if(!resJSON[0]) return
+     console.log('resJSON', resJSON)
+     this.setState({
+       ...this.state,
+       books: resJSON
+     })
+   } else {
+     console.log('Error:', response)
+     throw new Error('GET request failed')
+   }
+ } 
+
+
+  matchingBook = searchInput => {
+    this.setState({
+      ...this.state,
+      searchInput
+    })
+  }
+
+
   render() {
     return (
       <main>
-        <SearchBar />
+        <SearchBar searchFor={this.searchFor}/>
       </main>
-    )
+    );
   }
 }
 
